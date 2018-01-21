97575cce9a24dfc8d753d429bad9de20eb11ca8b
hooni <hooni@DESKTOP-6C1TFIC>
2018-01-21 13:37:41 +0900

    로그아웃 뒤로가기

--- SPRING_Project_TCAT/src/main/java/spring/project/tcat/controller/JMHGuestController.java
+++ SPRING_Project_TCAT/src/main/java/spring/project/tcat/controller/JMHGuestController.java
@@ -13,15 +13,15 @@ import spring.project.tcat.service.JMHGuestService;
 public class JMHGuestController {
 	@Autowired
 	JMHGuestService mhService;
-	
+
 	//////// 보류단계
-	//memberFind
+	// memberFind
 	@RequestMapping("memberFind")
 	public String memberFind(HttpServletRequest req, Model model) {
 		System.out.println("memberFind");
 		return "tcat/memberFind/memberFind";
 	}
-	
+
 	// 아이디 비밀번호 찾지 이메일인증 코드 보내기
 	@RequestMapping("findId")
 	public String findIdPwd(HttpServletRequest req, Model model) {
@@ -29,68 +29,72 @@ public class JMHGuestController {
 		mhService.findIdPwd(req, model);
 		return "tcat/memberFind/findId";
 	}
-	
-	//인증번호 체크
+
+	// 인증번호 체크
 	@RequestMapping("findResult")
 	public String findResult(HttpServletRequest req, Model model) {
 		System.out.println("findResult");
-		//결과에 따라 주소값결정
-		mhService.findResult(req,model);
+		// 결과에 따라 주소값결정
+		mhService.findResult(req, model);
 		return "tcat/memberFind/findId";
 	}
+
 	@RequestMapping("findResultPage")
 	public String findResultPage(HttpServletRequest req, Model model) {
 		System.out.println("findResultPage");
-		System.out.println(req.getParameter("member_id")+"/"+req.getParameter("member_pwd"));
+		System.out.println(req.getParameter("member_id") + "/" + req.getParameter("member_pwd"));
 		model.addAttribute("member_id", req.getParameter("member_id"));
 		model.addAttribute("member_pwd", req.getParameter("member_pwd"));
 		return "tcat/memberFind/findResultPage";
 	}
-	
+
 	/////////////////////////////////////////////////////////////
-	//서브메뉴 및 ajax div
+	// 서브메뉴 및 ajax div
 	@RequestMapping("myPage")
 	public String myPage(HttpServletRequest req, Model model) {
 		System.out.println("myPage");
 		return "tcat/memberMyPage/myPage";
 	}
-	//마이페이지 메인
+
+	// 마이페이지 메인
 	@RequestMapping("myPageMain")
 	public String myPageMain(HttpServletRequest req, Model model) {
 		System.out.println("myPageMain");
-		//해당 아이디 정보 가져오기
-		mhService.myPageInfo(req,model);
+		// 해당 아이디 정보 가져오기
+		mhService.myPageInfo(req, model);
 		return "tcat/memberMyPage/myPageMain";
 	}
-	
-	
+
 	//////////////////////////////////////////////////////////////
-	//memberLogin 모달 로그인 창
+	// memberLogin 모달 로그인 창
 	@RequestMapping("memberLogin")
 	public String memberLogin(HttpServletRequest req, Model model) {
 		System.out.println("memberLogin");
 		return "tcat/memberLogin/memberLogin";
 	}
-	//로그인 처리 loginPro
+
+	// 로그인 처리 loginPro
 	@RequestMapping("loginPro")
 	public String loginPro(HttpServletRequest req, Model model) {
 		System.out.println("loginPro");
-		mhService.loginPro(req,model);
+		mhService.loginPro(req, model);
 		return "tcat/memberLogin/loginPro";
 	}
-	//로그아웃
+
+	// 로그아웃
 	@RequestMapping("logout")
 	public String logout(HttpServletRequest req, Model model) {
 		System.out.println("logout");
 		req.getSession().removeAttribute("login_id");
 		return "tcat/guestMain/guestPage";
 	}
+
 	////////////////////////////////////////////////////////////////////
-	//licenseMusical 뮤지컬-라이센스
+	// licenseMusical 뮤지컬-라이센스
 	@RequestMapping("licenseMusical")
 	public String licenseMusical(HttpServletRequest req, Model model) {
 		System.out.println("licenseMusical");
-		mhService.getPerfList(req,model);
+		mhService.getPerfList(req, model);
 		return "tcat/musical/licenseMusical";
 	}
 }
