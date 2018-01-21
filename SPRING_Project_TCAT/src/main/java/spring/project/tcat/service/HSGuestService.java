b10c6d5fbc775715c37015a0f29da9aeecd2c65b
현석 <@kimHyeonSeok>
2018-01-21 13:17:55 +0900

    commit[김현석]-18/01/21

--- SPRING_Project_TCAT/src/main/java/spring/project/tcat/service/HSGuestService.java
+++ SPRING_Project_TCAT/src/main/java/spring/project/tcat/service/HSGuestService.java
@@ -9,4 +9,11 @@ public interface HSGuestService {
 	public void MemberIdCheack(HttpServletRequest req,Model model);
 	//게스트 가입
 	public void insertMember(HttpServletRequest req,Model model);
+	//날짜별 리스트 가져오기
+	public void daySearch(HttpServletRequest req,Model model);
+	//지역별 리스트 가져오기
+	public void placeList(HttpServletRequest req,Model model);
+	//장바구니에 담기
+	public void insertCart(HttpServletRequest req,Model model);
+	
 }
